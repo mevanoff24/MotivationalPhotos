085a95a3b84fdb20fecffa62665c6f944146a1f7
Matt Evanoff <mevanoff24@gmail.com>
2015-04-22 20:10:16 -0700

    5 TDD tests

--- motivational_photo/app/controllers/posts_controller.rb
+++ motivational_photo/app/controllers/posts_controller.rb
@@ -8,15 +8,38 @@ class PostsController < ApplicationController
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def create
   	@post = Post.new(title: params[:post][:title], image: params[:post][:image], description: params[:post][:description])
 
   	if @post.save
-  		redirect_to posts_path, notice: "Successfully added post"
+  		redirect_to posts_path, notice: "Successfully Added Post"
   	else
   		redirect_to :back, notice: "Failed to Add Post"
+  	end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	if @post.update(title: params[:post][:title], image: params[:post][:image], description: params[:post][:description])
+  		redirect_to posts_path, notice: "Successfully Updated Post"
+  	else
+  		redirect_to :back, notice: "Failed to Update Post"
+  	end
+  end
+
+  def destroy
+  	@post = Post.find(params[:id])
+  	if @post.delete 
+  		redirect_to posts_path, notice: "Successfully Deleted Post"
+  	else
+  		redirect_to :back
   	end
   end
 end
--- motivational_photo/spec/features/posts_spec.rb
+++ motivational_photo/spec/features/posts_spec.rb
@@ -15,10 +15,37 @@ describe "Posts" do
     	visit posts_path
     	click_link "Create New Post"
     	fill_in "Title", with: "new post"
+    	fill_in "Image", with: "example.png"
     	fill_in "Description", with: "new description"
     	click_button "Create Post"
     	expect(page).to have_content("new post")
-    	expect(page).to have_content("Successfully added post")
+    	expect(page).to have_content("Successfully Added Post")
     end
+
+    it "can edit a task" do 
+    	visit posts_path
+    	click_link "Edit"
+    	expect(page).to have_selector("input[value='poster']")
+    	fill_in "Title", with: "updated title"
+    	click_button "Update Post"
+    	expect(page).to have_content("updated title")
+    	expect(page).to have_content("Successfully Updated Post")
+    end
+
+    it "should not update a blank post" do 
+    	visit posts_path
+    	click_link "Edit"
+    	fill_in "Title", with: ""
+    	click_button "Update Post"
+    	expect(page).to have_content("Failed to Update Post")
+    end
+  end
+
+  describe "GET /post" do 
+  	it "can delete a post" do 
+  		visit post_path(@post)
+  		click_link "Delete"
+  		expect(page).to have_content("Successfully Deleted Post")
+  	end
   end
 end
