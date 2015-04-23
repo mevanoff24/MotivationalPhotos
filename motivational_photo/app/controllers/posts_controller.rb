d3af121dd95cc1826aa76e60813880e30ae7b48c
Matt Evanoff <mevanoff24@gmail.com>
2015-04-23 09:16:24 -0700

    gem and prvite method

--- motivational_photo/app/controllers/posts_controller.rb
+++ motivational_photo/app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   	@posts = Post.all
   end
@@ -8,12 +11,10 @@ class PostsController < ApplicationController
   end
 
   def show
-  	@post = Post.find(params[:id])
   end
 
   def create
-  	@post = Post.new(title: params[:post][:title], image: params[:post][:image], description: params[:post][:description])
-
+  	@post = Post.new(post_params)
   	if @post.save
   		redirect_to posts_path, notice: "Successfully Added Post"
   	else
@@ -22,11 +23,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-  	@post = Post.find(params[:id])
   end
 
   def update
-  	@post = Post.find(params[:id])
   	if @post.update(title: params[:post][:title], image: params[:post][:image], description: params[:post][:description])
   		redirect_to posts_path, notice: "Successfully Updated Post"
   	else
@@ -35,11 +34,20 @@ class PostsController < ApplicationController
   end
 
   def destroy
-  	@post = Post.find(params[:id])
   	if @post.delete 
   		redirect_to posts_path, notice: "Successfully Deleted Post"
   	else
   		redirect_to :back
   	end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :image, :description)
   end
 end
